334a043ed371e8e3786ded4196b39250fb34de6a
kp <kakkunpakkun@users.noreply.github.com>
2021-06-07 22:02:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const repository = core.getInput('repository');
 var owner = core.getInput('owner');
 var repo = core.getInput('repo');
 var excludes = core.getInput('excludes').trim().split(",");
+const token = coree.getInput('token');
 
-const octokit = new Octokit()
+const octokit = new Octokit({auth: token})
 
 async function run() {
     try {
